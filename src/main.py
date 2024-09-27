cd56b92dcd526712f0f524d76ff16d9dee3256e7
Nikolai Petukhov <petukhov.niko@gmail.com>
2024-09-27 03:38:33 -0300

    add static

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import supervisely as sly
 from src.ui import layout, load_dashboard
+import src.globals as g
 
-app = sly.Application(layout=layout)
+app = sly.Application(layout=layout, static_dir=g.TEMP_DATA_PATH)
 load_dashboard()
